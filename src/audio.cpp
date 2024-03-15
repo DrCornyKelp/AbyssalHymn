338580163d6404c2ada740ff25d98a760e34fbd3
DrCornyKelp <hieuanh01122004@gmail.com>
2024-03-15 17:28:02 +0700

    roll a dice

--- src/audio.cpp
+++ src/audio.cpp
@@ -2,7 +2,8 @@
 
 void Audio::playBGM(const char *wavPath)
 {
-    Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 1024);
+    // 22050, 44100, or 48000
+    Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 1024);
     Mix_Music *music = Mix_LoadMUS(wavPath);
     Mix_PlayMusic(music, 0);
 }
