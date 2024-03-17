ccdae2d1e492998329a1170898c13fac82a963a5
DrCornyKelp <hieuanh01122004@gmail.com>
2024-03-17 15:09:43 +0700

    roll a dice

--- main.cpp
+++ main.cpp
@@ -76,7 +76,7 @@ int main(int argc, char *argv[])
     staticFront.push_back(new DecorationStatic(game->getRenderer(), "res/BlockTile/Grass3.png", 1880, 320, 880, 640, false));
     // NPCS DIALOGUE
     std::vector<NpcDialogue *> npcs;
-    npcs.push_back(new NpcDialogue(920, 190, 128, 128, 200, 200, 480, 225, 32, 32, 2, 100, 0, 0, 230));
+    npcs.push_back(new NpcDialogue(920, 190, 128, 128, 200, 200, 480, 225, 32, 32, 4, 10, 0, 0, 230));
     npcs[0]->initNpc(game->getRenderer(), "res/NpcDialogue/Npc2/Squid.png", "res/NpcDialogue/Npc2/DemoNakuBubble1.png");
 
     // Game loop
