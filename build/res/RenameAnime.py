750bac17a1f9f636fa98ddb1a3a28a8d07f3617a
DrCornyKelp <hieuanh01122004@gmail.com>
2024-05-15 19:52:20 +0700

    don't blame me

--- build/res/RenameAnime.py
+++ build/res/RenameAnime.py
@@ -7,9 +7,6 @@ to suit the cpp decoration animation directory
 import os
 
 directory_path = 'Menu/Background/'
-old_part_1 = "frame_"
-old_part_2 = "_delay-0.03s";
-old_type = ".gif"
 
 def convert(number, max_number):
     num_digits = len(str(max_number))
@@ -19,9 +16,9 @@ def rename_files(directory):
     files = os.listdir(directory)
 
     f_len = len(files)
-    for f in range(f_len):
-        os.rename(
-            f"{directory}{old_part_1}{convert(f, f_len)}{old_part_2}{old_type}",
-            f"{directory}frame_{convert(f, f_len)}.png")
+    f = 0
+    for filename in files:
+        os.rename(f"{directory}{filename}", f"{directory}frame_{convert(f, f_len)}.png")
+        f += 1
 
 rename_files(directory_path)
